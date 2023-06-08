5e0307622368a2e0fd3e8a635ebfc5b21543fd25
yhjs1211 <122883378+yhjs1211@users.noreply.github.com>
2023-06-09 08:03:18 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,8 +1,8 @@
 function solution(array, n) {
     let answer = array[0];
     for(let i=1; i<array.length; i++){
-        let num1 = n-array[i]<0 ? (n-array[i])*-1 : n-array[i];
-        let num2 = n-answer<0 ? (n-answer)*-1 : n-answer;
+        let num1 = Math.abs(n-array[i]);
+        let num2 = Math.abs(n-answer);
 
         if(num1<num2){
             answer=array[i];
