83716f64da8c09a3e38d251e69b36fb6de39787f
Jung Younghoon <yhjs1211@gmail.com>
2023-09-20 23:50:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    for(let i=0; i<signs.length; i++){
+        signs[i]
+            ? answer+=absolutes[i]
+            : answer+=absolutes[i]*-1
+    }
+    return answer;
+}
