3627ec9dabb5e07176297d185d0ef1bf57369b92
Jung Younghoon <yhjs1211@gmail.com>
2023-09-21 00:23:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,24 +1,11 @@
 function solution(arr) {
     let answer = [];
-    let min;
-    let minIdx;
-    if(arr.length==1){
-        answer.push(-1);
-    }else{
-        arr.forEach((v,idx)=>{
-            if(minIdx){
-                if(arr[minIdx]>v){
-                    minIdx=idx;     
-                }
-            }else{
-                minIdx=idx;
-            }
-        });
-        arr.forEach((v,idx)=>{
-        idx!==minIdx
-            ? answer.push(v)
-            : null
-        })    
-    }
+    const min = Math.min(...arr);
+    const len = arr.length;
+    len === 1
+        ? answer.push(-1)
+        : arr.forEach(v=>{
+        if(v!==min)answer.push(v)
+    })
     return answer;
 }
