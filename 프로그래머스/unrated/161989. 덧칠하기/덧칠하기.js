8f34d64ad20b09481174a4aa2ae3a21cc76ab406
Jung Younghoon <yhjs1211@gmail.com>
2023-09-21 23:26:59 +0900

    [unrated] Title: 덧칠하기, Time: 1.83 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,21 +1,26 @@
-function solution(n,m, section){
-    let answer=0;
-
-    let wallMin=section[0];
-    let wallMax=section[section.length-1];
-
-    let str = section.join('');
-
-    section.forEach(v=>{
-        let last = wallMin+m > n ? n+1 : wallMin+m;
-        if(wallMin<=v && v<last){
-            str=str.replace(String(v),'');
+function solution(n, m, section) {
+    let answer = 0;
+    let rangeMin;
+    let rangeMax;
+    
+    section.forEach((v)=>{
+        if(rangeMin && rangeMax){
+            if( rangeMax < v) {
+                rangeMin = v;
+                rangeMax = v+m-1;
+                
+                if(rangeMax>n){
+                    const sub = n-rangeMax;
+                    rangeMin+=sub;
+                    rangeMax+=sub;
+                }
+                answer++;
+            }
         }else{
+            rangeMin = v;
+            rangeMax = v+m-1;
             answer++;
-            str=str.replace(String(v),'');
-            wallMin=v;
         }
     })
-    answer++;
     return answer;
 }
