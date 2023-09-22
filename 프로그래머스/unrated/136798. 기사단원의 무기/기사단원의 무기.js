c80469cd0785ad54f69de5437c8212fac3a9bef9
Jung Younghoon <yhjs1211@gmail.com>
2023-09-22 21:21:40 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.70 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -13,23 +13,9 @@ function solution(number, limit, power) {
             }
         }
         const len = divArr.length;
-        if(len>limit)answer+=power;
-        answer+=len;
+        len > limit ? answer+=power : answer+=len;
         divArr=[];
     }
     
-    return answer;
-}
-
-function solution(number, limit, power) {
-    const numberArray = Array(number).fill(0).map((_,idx)=>idx+1)
-    const answer = numberArray.map(number=>{
-        let divideCount = 0
-        for(let i=1;i<Math.sqrt(number);i++){
-            (number % i === 0) && divideCount++
-        }
-        divideCount = (divideCount * 2 + Number.isInteger(Math.sqrt(number)))
-        return divideCount > limit ? power : divideCount
-    }).reduce((acc,val)=>acc+val,0)
     return answer;
 }
